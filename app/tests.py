ff651d86ab9844f7dcda876a73e00fde850434b3
pedropazs <pedrofelipek@gmail.com>
2025-06-27 00:47:39 -0300

    Testes básicos para rotas CRUD do backend

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import YourModel
 
-# Create your tests here.
+class YourModelTests(TestCase):
+
+    def setUp(self):
+        # Set up any initial data for the tests here
+        YourModel.objects.create(field_name='value')
+
+    def test_model_str(self):
+        # Test the string representation of the model
+        instance = YourModel.objects.get(field_name='value')
+        self.assertEqual(str(instance), 'Expected String Representation')
+
+    def test_model_field(self):
+        # Test a specific field of the model
+        instance = YourModel.objects.get(field_name='value')
+        self.assertEqual(instance.field_name, 'value')
